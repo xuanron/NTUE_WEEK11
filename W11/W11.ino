390187dfc67b8cbda78017fd6333129cbf4f3f3d
xuanron <83741471+xuanron@users.noreply.github.com>
2021-05-06 10:47:29 +0800

    Add files via upload

---
+++ W11/W11.ino
@@ -0,0 +1,19 @@
+int buttonState=0;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13,OUTPUT);
+pinMode(2,INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState=digitalRead(2);
+
+  if(buttonState==HIGH){
+    digitalWrite(13,HIGH);
+  }
+  else{
+    digitalWrite(13,LOW);
+  }
+}
